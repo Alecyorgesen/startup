25913e62954447e403f77aecc7d6eec069d0fdb0
alecyorgesen <alecyorgesen@gmail.com>
2024-11-05 23:45:59 -0700

    fixed some stuff :)

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import './about.css'
+import './about.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function About() {
     return (
         <div>About is Here!!!</div>
-    )
+    );
 }
--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
-import { Login } from './login/login';
-import { Play } from './play/play';
-import { Scores } from './scores/scores';
-import { About } from './about/about';
+import { BrowserRouter, NavLink, Route, Router, Routes } from 'react-router-dom';
+import Login from './login/login';
+import Play from './play/play';
+import Scores from './scores/scores';
+import About from './about/about';
 // import { AuthState } from './login/AuthState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
@@ -12,35 +12,41 @@ export default function App() {
 
     return (
         <BrowserRouter>
-            <header class="sticky-top">
-                <nav class="navbar navbar-expand-lg bg-grey">
-                    <div class="container-fluid">
+            <header className="sticky-top">
+                <nav className="navbar navbar-expand-lg bg-grey">
+                    <div className="container-fluid">
                     <img src="Rock Paper Scissors Showdown.png" alt="RPS Showdown" width="50" />
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.html">Login</a>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="index.html">Login</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="play.html">Play</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="play.html">Play</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="scores.html">Scores</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="scores.html">Scores</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.html">About the game</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="about.html">About the game</a>
                         </li>
                         </ul>
                     </div>
-                    <div class="d-flex ms-auto">
-                        <p class="mb-0 me-3">lDrac360l (Username)</p>
+                    <div className="d-flex ms-auto">
+                        <p className="mb-0 me-3">lDrac360l (Username)</p>
                     </div>
                     </div>
                 </nav>
             </header>
+
+            <Routes>
+                <Route>
+
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
