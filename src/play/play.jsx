7631031158c30f48a3a4191f009645500e3f5039
alecyorgesen <alecyorgesen@gmail.com>
2024-11-06 07:45:13 -0700

    made it so that bootstrap javascript works

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,101 @@
 import React from "react";
 import './play.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Play() {
     return (
-        <div>Play is here!</div>
+    <main>
+        <nav className="d-flex justify-content-center">
+            <button className="btn btn-primary m-2 button">Find random match</button>
+            <button className="btn btn-primary m-2 button">Play against a friend</button>
+        </nav>
+        <div className="row">
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="gray_square.png" alt="gray_square" width="100px" />
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="gray_square.png" alt="gray_square" width="100px" />
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="gray_square.png" alt="gray_square" width="100px" />
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="gray_square.png" alt="gray_square" width="100px" />
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="gray_square.png" alt="gray_square" width="100px" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="dropdown col d-flex justify-content-center">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Select
+            </button>
+            <ul className="dropdown-menu">
+              <li><button className="dropdown-item" type="button">Rock</button></li>
+              <li><button className="dropdown-item" type="button">Paper</button></li>
+              <li><button className="dropdown-item" type="button">Scissors</button></li>
+            </ul>
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Select
+            </button>
+            <ul className="dropdown-menu">
+              <li><button className="dropdown-item" type="button">Rock</button></li>
+              <li><button className="dropdown-item" type="button">Paper</button></li>
+              <li><button className="dropdown-item" type="button">Scissors</button></li>
+            </ul>
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Select
+            </button>
+            <ul className="dropdown-menu">
+              <li><button className="dropdown-item" type="button">Rock</button></li>
+              <li><button className="dropdown-item" type="button">Paper</button></li>
+              <li><button className="dropdown-item" type="button">Scissors</button></li>
+            </ul>
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Select
+            </button>
+            <ul className="dropdown-menu">
+              <li><button className="dropdown-item" type="button">Rock</button></li>
+              <li><button className="dropdown-item" type="button">Paper</button></li>
+              <li><button className="dropdown-item" type="button">Scissors</button></li>
+            </ul>
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Select
+            </button>
+            <ul className="dropdown-menu">
+              <li><button className="dropdown-item" type="button">Rock</button></li>
+              <li><button className="dropdown-item" type="button">Paper</button></li>
+              <li><button className="dropdown-item" type="button">Scissors</button></li>
+            </ul>
+          </div>
+        </div>
+        <div className="row">
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="gray_square.png" alt="gray_square" width="100px" />
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="rock.png" alt="rock" width="100px" />
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="scissors.png" alt="scissors" width="100px" />
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="paper.png" alt="paper" width="100px" />
+          </div>
+          <div className="dropdown col d-flex justify-content-center">
+            <img src="rock.png" alt="rock" width="100px" />
+          </div>
+        </div>
+    </main>
     )
 }
