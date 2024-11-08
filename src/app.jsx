f11400766ebc5ad735f30ec22fd982a75420663f
alecyorgesen <alecyorgesen@gmail.com>
2024-11-07 19:05:52 -0700

    added prettier

--- src/app.jsx
+++ src/app.jsx
@@ -1,98 +1,138 @@
-import React from 'react';
-import { useState } from 'react';
-import { BrowserRouter, NavLink, Route, Router, Routes } from 'react-router-dom';
-import Login from './login/login';
-import Play from './play/play';
-import Scores from './scores/scores';
-import About from './about/about';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; //I might change this later...
-import './app.css';
+import React from "react";
+import { useState } from "react";
+import {
+  BrowserRouter,
+  NavLink,
+  Route,
+  Router,
+  Routes,
+} from "react-router-dom";
+import Login from "./login/login";
+import Play from "./play/play";
+import Scores from "./scores/scores";
+import About from "./about/about";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; //I might change this later...
+import "./app.css";
 
 export default function App() {
-    const [username, setUsername] = useState('');
-    const [authenticated, setAuthenticated] = useState(false);
-    const [gameInProgress, setGameInProgress] = useState(false);
-    let list = []
-    for (let i = 0; i < 10; i++) {
-      list.push("gray_square.png")
-    }
-    const [pngList, setPngList] = useState(list)
+  const [username, setUsername] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
+  const [gameInProgress, setGameInProgress] = useState(false);
+  let list = [];
+  for (let i = 0; i < 10; i++) {
+    list.push("gray_square.png");
+  }
+  const [pngList, setPngList] = useState(list);
 
-    return (
-        <BrowserRouter>
-        <div className='body'>
-            <header className="sticky-top">
-                <nav className="navbar navbar-expand-lg bg-grey">
-                    <div className="container-fluid">
-                    <img src="Rock Paper Scissors Showdown.png" alt="RPS Showdown" width="50" />
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="">Login</NavLink>
-                        </li>
-                        {authenticated === true && (
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="play">Play</NavLink>
-                            </li>
-                        )}
-                        {authenticated === true && (
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="scores">Scores</NavLink>
-                            </li>
-                        )}
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="about">About the game</NavLink>
-                        </li>
-                        </ul>
-                    </div>
-                    <div className="d-flex ms-auto">
-                        <p className="mb-0 me-3">{username}</p>
-                    </div>
-                    </div>
-                </nav>
-            </header>
+  return (
+    <BrowserRouter>
+      <div className="body">
+        <header className="sticky-top">
+          <nav className="navbar navbar-expand-lg bg-grey">
+            <div className="container-fluid">
+              <img
+                src="Rock Paper Scissors Showdown.png"
+                alt="RPS Showdown"
+                width="50"
+              />
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="">
+                      Login
+                    </NavLink>
+                  </li>
+                  {authenticated === true && (
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="play">
+                        Play
+                      </NavLink>
+                    </li>
+                  )}
+                  {authenticated === true && (
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="scores">
+                        Scores
+                      </NavLink>
+                    </li>
+                  )}
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="about">
+                      About the game
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+              <div className="d-flex ms-auto">
+                <p className="mb-0 me-3">{username}</p>
+              </div>
+            </div>
+          </nav>
+        </header>
 
-            <Routes>
-                <Route path='/' element={<Login
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Login
                 username={username}
                 setUsername={setUsername}
                 authenticated={authenticated}
                 setAuthenticated={setAuthenticated}
-                />} />
-                <Route path='/play' element={<Play 
+              />
+            }
+          />
+          <Route
+            path="/play"
+            element={
+              <Play
                 gameInProgress={gameInProgress}
                 setGameInProgress={setGameInProgress}
                 pngList={pngList}
                 setPngList={setPngList}
-                />} />
-                <Route path='/scores' element={<Scores />} />
-                <Route path='/about' element={<About />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
+              />
+            }
+          />
+          <Route path="/scores" element={<Scores />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
-
-            <footer className="d-flex background-grey position-absolute bottom-0 end-0">
-            <div className="container-fluid">
-              <div className="row">
-                  <div className="col d-flex justify-content-center">
-                      <p>Alec Yorgesen</p>
-                  </div>
-                  <div className="col d-flex justify-content-center fw-bold">
-                      <a href="https://github.com/Alecyorgesen/startup">Github Link</a>
-                  </div>
+        <footer className="d-flex background-grey position-absolute bottom-0 end-0">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col d-flex justify-content-center">
+                <p>Alec Yorgesen</p>
+              </div>
+              <div className="col d-flex justify-content-center fw-bold">
+                <a href="https://github.com/Alecyorgesen/startup">
+                  Github Link
+                </a>
               </div>
             </div>
+          </div>
         </footer>
-        </div>
-        </BrowserRouter>
-        
-    )
+      </div>
+    </BrowserRouter>
+  );
 }
 
 function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
-  }
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      404: Return to sender. Address unknown.
+    </main>
+  );
+}
--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,72 +1,124 @@
 import React from "react";
-import './play.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./play.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-export default function Play({ gameInProgess, setGameInProgress, pngList, setPngList }) {
-    let alt = "gray_squre";
-    return (
+export default function Play({
+  gameInProgess,
+  setGameInProgress,
+  pngList,
+  setPngList,
+}) {
+  let alt = "gray_squre";
+  if (gameInProgess) {
+    console.log("hey there sir!!!");
+  }
+  return (
     <main>
+      {gameInProgess === false && (
         <nav className="d-flex justify-content-center">
-            <button className="btn btn-primary m-2 button">Find random match</button>
-            <button className="btn btn-primary m-2 button">Play against a friend</button>
+          <button
+            className="btn btn-primary m-2 button"
+            onClick={() => startGame()}
+          >
+            Find random match
+          </button>
+          <button
+            className="btn btn-primary m-2 button"
+            onClick={() => startGame()}
+          >
+            Play against a friend
+          </button>
         </nav>
-        <div className="row p-5">
-          <Image png={pngList[5]} alt={alt} />
-          <Image png={pngList[6]} alt={alt} />
-          <Image png={pngList[7]} alt={alt} />
-          <Image png={pngList[8]} alt={alt} />
-          <Image png={pngList[9]} alt={alt} />
-        </div>
+      )}
 
-        <div className="row pt-5">
-          {gameInProgess === true && (
-            <>
-              <Dropdown number={0} />
-              <Dropdown number={1} />
-              <Dropdown number={2} />
-              <Dropdown number={3} />
-              <Dropdown number={4} />
-            </>
-          )}
-        </div>
+      <div className="row p-5">
+        <Image png={pngList[5]} alt={alt} />
+        <Image png={pngList[6]} alt={alt} />
+        <Image png={pngList[7]} alt={alt} />
+        <Image png={pngList[8]} alt={alt} />
+        <Image png={pngList[9]} alt={alt} />
+      </div>
 
-        <div className="row pb-5">
-          <Image png={pngList[0]} alt={alt} />
-          <Image png={pngList[1]} alt={alt} />
-          <Image png={pngList[2]} alt={alt} />
-          <Image png={pngList[3]} alt={alt} />
-          <Image png={pngList[4]} alt={alt} />
-        </div>
+      <div className="row pt-5">
+        {gameInProgess === true && (
+          <>
+            <Dropdown number={0} />
+            <Dropdown number={1} />
+            <Dropdown number={2} />
+            <Dropdown number={3} />
+            <Dropdown number={4} />
+          </>
+        )}
+      </div>
+
+      <div className="row pb-5">
+        <Image png={pngList[0]} alt={alt} />
+        <Image png={pngList[1]} alt={alt} />
+        <Image png={pngList[2]} alt={alt} />
+        <Image png={pngList[3]} alt={alt} />
+        <Image png={pngList[4]} alt={alt} />
+      </div>
     </main>
-    )
+  );
+  function startGame() {
+    setGameInProgress(true);
+  }
 
-    function Image({ png, alt }) {
-      return (
+  function Image({ png, alt }) {
+    return (
       <div className="col d-flex justify-content-center">
         <img src={png} alt={alt} width="100px" />
       </div>
-      )
-    }
-    
-    function Dropdown({ number }) {
-      return (
-        <div className="dropdown col d-flex justify-content-center">
-          <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Select
-          </button>
-          <ul className="dropdown-menu">
-            <li><button className="dropdown-item" type="button" onClick={() => setElementInPngList(number, 'rock.png')}>Rock</button></li>
-            <li><button className="dropdown-item" type="button" onClick={() => setElementInPngList(number, 'paper.png')}>Paper</button></li>
-            <li><button className="dropdown-item" type="button" onClick={() => setElementInPngList(number, 'scissors.png')}>Scissors</button></li>
-          </ul>
-        </div>
-      )
-    }
-    
-    function setElementInPngList(i, type) {
-      let newList = [...pngList];
-      newList[i] = type;
-      setPngList(newList);
-    }
+    );
+  }
+
+  function Dropdown({ number }) {
+    return (
+      <div className="dropdown col d-flex justify-content-center">
+        <button
+          className="btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Select
+        </button>
+        <ul className="dropdown-menu">
+          <li>
+            <button
+              className="dropdown-item"
+              type="button"
+              onClick={() => setElementInPngList(number, "rock.png")}
+            >
+              Rock
+            </button>
+          </li>
+          <li>
+            <button
+              className="dropdown-item"
+              type="button"
+              onClick={() => setElementInPngList(number, "paper.png")}
+            >
+              Paper
+            </button>
+          </li>
+          <li>
+            <button
+              className="dropdown-item"
+              type="button"
+              onClick={() => setElementInPngList(number, "scissors.png")}
+            >
+              Scissors
+            </button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  function setElementInPngList(i, type) {
+    let newList = [...pngList];
+    newList[i] = type;
+    setPngList(newList);
+  }
 }
