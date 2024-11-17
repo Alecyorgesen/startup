d014bc252baa8bff3744d8470e4aa4c3fc7a5ece
alecyorgesen <alecyorgesen@gmail.com>
2024-11-16 17:27:43 -0700

    added stuff to scores and to about

--- src/scores/scores.jsx
+++ src/scores/scores.jsx
@@ -3,39 +3,43 @@ import './scores.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Scores() {
+    const [scores, setScores] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('/api/scores')
+            .then((response) => response.json())
+            .then((scores) => {
+                setScores(scores);
+            });
+    }, []);
+    let scoreRows = [];
+    for (index in scores) {
+        scoreRows.push(<Score index={index} />)
+    }
+
     return (
-    <main>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Wins</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>lDrac360l</td>
-                    <td>36</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>yorgiwarrenater</td>
-                    <td>32</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>RockCastle808</td>
-                    <td>31</td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>Dragonnmaster7</td>
-                    <td>-20</td>
-                </tr>
-            </tbody>
-        </table>
-    </main>
+        <main>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Wins</th>
+                    </tr>
+                </thead>
+                <tbody>{scoreRows}</tbody>
+            </table>
+        </main>
     );
+}
+
+function Score({ index }) {
+
+    return (
+        <tr>
+            <td>{index + 1}</td>
+            <td>{scores[index].name}</td>
+            <td>{scores[index].score}</td>
+        </tr>
+    )
 }
